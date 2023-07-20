5e30247b452fe1153dbff7b0303bc9ddddf9c0ea
leticea <nascimento.leticeah@gmail.com>
2023-07-20 17:31:48 -0300

    feat(script.js): js codes

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ sideLinks.forEach((item) => {
   const li = item.parentElement;
   item.addEventListener("click", () => {
     sideLinks.forEach((i) => {
-      i.parentElement.classList.remove(".active");
+      i.parentElement.classList.remove("active");
     });
     li.classList.add("active");
   });
 });
 
-const menuBar = document.querySelector(".content nav .b.bx-menu");
+const menuBar = document.querySelector(".content nav .bx.bx-menu");
 const sideBar = document.querySelector(".sidebar");
 
 menuBar.addEventListener("click", () => {
@@ -25,3 +25,37 @@ const searchBtn = document.querySelector(
 const searchBtnIcon = document.querySelector(
   ".content nav form .form-input button .bx"
 );
+const searchForm = document.querySelector(".content nav form");
+
+searchBtn.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault;
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchBtnIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchBtnIcon.classList.replace("bx-x", "bx-search");
+    }
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    sideBar.classList.add("close");
+  } else {
+    sideBar.classList.remove("close");
+  }
+
+  if (window.innerWidth > 576) {
+    searchBtnIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
+});
+
+const toggler = document.getElementById("theme-toggle");
+
+toggler.addEventListener("change", function () {
+  this.checked
+    ? document.body.classList.add("dark")
+    : document.body.classList.remove("dark");
+});
