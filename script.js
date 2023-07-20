4ff5f64699ffaafffeb53786546ee60c4980e58a
leticea <nascimento.leticeah@gmail.com>
2023-07-20 17:09:08 -0300

    feat(script.js): js codes

---
+++ script.js
@@ -0,0 +1,27 @@
+const sideLinks = document.querySelectorAll(
+  ".sidebar .side-menu li a:not(.logout)"
+);
+
+sideLinks.forEach((item) => {
+  const li = item.parentElement;
+  item.addEventListener("click", () => {
+    sideLinks.forEach((i) => {
+      i.parentElement.classList.remove(".active");
+    });
+    li.classList.add("active");
+  });
+});
+
+const menuBar = document.querySelector(".content nav .b.bx-menu");
+const sideBar = document.querySelector(".sidebar");
+
+menuBar.addEventListener("click", () => {
+  sideBar.classList.toggle("close");
+});
+
+const searchBtn = document.querySelector(
+  ".content nav form .form-input button"
+);
+const searchBtnIcon = document.querySelector(
+  ".content nav form .form-input button .bx"
+);
